4b5bfa42fa36bbd4df7488fbc8963683360849d6
Use <usemartin2@gmail.com>
2024-03-05 20:03:18 +0100

    componente clima

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { RouterOutlet, Routes } from '@angular/router';
+import { ClimaComponent } from './pages/clima/clima.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{path: '', component:HomeComponent, title: 'Inicio'},
+{path: 'clima', component:ClimaComponent, title: 'Clima'},
+
+{path: '**',redirectTo: '',pathMatch:'full' },
+
+];
