b71164838d2798e2c7a24173e55405061a0217d3
Use <usemartin2@gmail.com>
2024-03-06 16:43:32 +0100

    commit

--- src/app/pages/rickandmorty/rickandmorty.component.ts
+++ src/app/pages/rickandmorty/rickandmorty.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RickandmortyService } from 'src/app/services/rickandmorty.service';
 
 @Component({
   selector: 'app-rickandmorty',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './rickandmorty.component.css'
 })
 export class RickandmortyComponent {
-
+  constructor(apiService: RickandmortyService) { }
+  info:any = ""
+  characters:any = []
+  ngOnInit(): void {
+    this.apiService.get().subscribe((r:any)=>{
+      this.characters = r.results
+      this.info = r.info
+    })
+  }
+  redirectTo(url:string){
+    this.apiService.getByUrl(url).subscribe((r:any)=>{
+      console.log(r.results)
+      this.characters = r.results
+      this.info = r.info
+    })
+  }
 }
