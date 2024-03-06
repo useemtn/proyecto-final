700078615ec770e2ea9ede48cedcae44006d59bd
Use <usemartin2@gmail.com>
2024-03-06 13:17:22 +0100

    arreglo fallos service and component

--- src/app/pages/github/github.component.ts
+++ src/app/pages/github/github.component.ts
@@ -1,34 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ProfileService } from '../../services/profile.service';
+import { GithubService } from '../../services/github.service';
 
 @Component({
   selector: 'gg-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-  profile:any[];
+export class GithubComponent implements OnInit {
+  profile: any[];
   repos: any[];
-  username:string;
+  username: string;
 
-  constructor(private profileService: ProfileService) { 
+  constructor(private githubService: GithubService) { }
 
-  }
+  findProfile() {
+    this.githubService.updateProfile(this.username);
+    this.githubService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
 
-  findProfile(){
-  	this.profileService.updateProfile(this.username);
-  	this.profileService.getProfileInfo().subscribe(profile => {
-  		console.log(profile);
-  		this.profile = profile;
-  	});
-
-  	this.profileService.getProfileRepos().subscribe(repos => {
-  		console.log(repos);
-  		this.repos = repos;
-  	})  	
+    this.githubService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
   }
 
   ngOnInit() {
   }
-
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,31 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class ProfileService {
-
-  private username:string;
+export class GithubService {
+  private username: string;
   private clientid = 'f3571d8e55e169540825';
   private clientsecret = 'a6c2b7d8f7c691e771b1908873e1322028e097c4';
 
-  constructor(private http:Http) { 
-  	console.log("service is now ready!");
-  	this.username = 'kirandash';
+  constructor(private http: HttpClient) {
+    console.log("service is now ready!");
+    this.username = 'kirandash';
   }
 
-  getProfileInfo(){
-  	return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-  	.map(res => res.json());
+  getProfileInfo(): Observable<any> {
+    return this.http.get(`https://api.github.com/users/${this.username}?client_id=${this.clientid}&client_secret=${this.clientsecret}`)
+      .pipe(map(res => res));
   }
 
-  getProfileRepos(){
-  	return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-  	.map(res => res.json());
+  getProfileRepos(): Observable<any> {
+    return this.http.get(`https://api.github.com/users/${this.username}/repos?client_id=${this.clientid}&client_secret=${this.clientsecret}`)
+      .pipe(map(res => res));
   }
 
-  updateProfile(username:string){
-  	this.username = username;
+  updateProfile(username: string): void {
+    this.username = username;
   }
-
 }
